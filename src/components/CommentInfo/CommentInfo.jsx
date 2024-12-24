b954a4ba7646d634c04748cf43616baa448d133b
Oleksii-Ivan Stetsyk <farshaleks@gmail.com>
2024-12-24 19:04:09 +0200

    react static list

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,1 +1,13 @@
-export const CommentInfo = () => <>Put the comment here</>;
+import { UserInfo } from '../UserInfo/UserInfo';
+import './CommentInfo.scss';
+
+export const CommentInfo = ({ comment }) => (
+  <div className="CommentInfo">
+    <div className="CommentInfo__title">
+      <strong className="CommentInfo__name">{comment.name}</strong>
+      {' by '}
+      <UserInfo user={{ name: comment.name, email: comment.email }} />
+    </div>
+    <div className="CommentInfo__body">{comment.body}</div>
+  </div>
+);
