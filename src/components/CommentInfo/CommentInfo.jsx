46b232ba70b8f2964ef450cb890475532091a1be
Oleksii-Ivan Stetsyk <farshaleks@gmail.com>
2024-12-26 15:28:37 +0200

    another one

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,4 +1,3 @@
-import { UserInfo } from '../UserInfo/UserInfo';
 import './CommentInfo.scss';
 
 export const CommentInfo = ({ comment }) => (
@@ -6,7 +5,9 @@ export const CommentInfo = ({ comment }) => (
     <div className="CommentInfo__title">
       <strong className="CommentInfo__name">{comment.name}</strong>
       {' by '}
-      <UserInfo user={{ name: comment.name, email: comment.email }} />
+      <a className="CommentInfo__email" href={`mailto:${comment.email}`}>
+        {comment.email}
+      </a>
     </div>
     <div className="CommentInfo__body">{comment.body}</div>
   </div>
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -2,6 +2,6 @@ import './UserInfo.scss';
 
 export const UserInfo = ({ comment }) => (
   <a className="CommentInfo__email" href={`mailto:${comment.email}`}>
-    {comment.email}
+    {comment.name}
   </a>
 );
